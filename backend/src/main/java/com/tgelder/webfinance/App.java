ea29e942cb8b0a3b422e6c7401127ad47d719a27
Thomas Elder <tgelder@gmail.com>
2018-07-12 22:47:24 +0100

    Small bug fixes

--- backend/src/main/java/com/tgelder/webfinance/App.java
+++ backend/src/main/java/com/tgelder/webfinance/App.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @Slf4j
@@ -18,6 +19,7 @@ public class App {
   }
 
   @Bean
+  @Profile("!test")
   public CommandLineRunner populateUsers(Config config, AuthorizedUserRepository authorizedUserRepository) {
     return args -> {
       authorizedUserRepository.deleteAll();
